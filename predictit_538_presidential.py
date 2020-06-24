ef11ab8d2ed8bed131ea3b0783f87dc9975b0b29
mauricebransfield <mauricebransfield@users.noreply.github.com>
2020-06-24 11:13:43 -0400

    updated odds

--- predictit_538_presidential.py
+++ predictit_538_presidential.py
@@ -1,106 +1,20 @@
 # TO DO
-# 1. Clarify column names
-# 2. Fix workaround in nan values for implied probability
-# 3. Create key by parsing PredictIt presidential market/contracts
-# 4. Fair probability
+# 1. Add additional odds
+# 2. bestBuyNoCost
+# 3. Fix workaround in nan values for implied probability
+# 4. Dashboard
+# 5. Fair probability
 
 # Import modules
 import json
 import requests
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas as pd
 pd.set_option('display.max_rows', None) #print all rows without truncating
 import numpy as np
 import datetime
 import os
-
-# Create unique key to link 538 polls with PredictIt markets
-states = [[6212, 'Biden', 22223],   #AL
-        [6212, 'Trump', 22222],     #AL
-        [6211, 'Biden', 22094], #AK
-        [6211, 'Trump', 22093], #AK
-        [6214, 'Biden', 16000], #AZ
-        [6214, 'Trump', 15999], #AZ
-        [6215, 'Biden', 22160], #CA
-        [6215, 'Trump', 22159], #CA
-        [6216, 'Biden', 16018], #CO
-        [6216, 'Trump', 16017], #CO
-        [6217, 'Biden', 22081], #CT
-        [6217, 'Trump', 22080], #CT
-        [6219, 'Biden', 22264], #DE 
-        [6219, 'Trump', 22263], #DE
-        [6220, 'Biden', 15707], #FL
-        [6220, 'Trump', 15706], #FL
-        [6221, 'Biden', 16016], #GA
-        [6221, 'Trump', 16015], #GA
-        [6226, 'Biden', 22034], #IN
-        [6226, 'Trump', 22033], #IN
-        [6223, 'Biden', 16013], #IA
-        [6223, 'Trump', 16014], #IA
-        [6227, 'Biden', 22227], #KS
-        [6227, 'Trump', 22226], #KS
-        [6228, 'Biden', 22096], #KY
-        [6228, 'Trump', 22095], #KY
-        [6232, 'Biden', 22032], #ME
-        [6232, 'Trump', 22031], #ME
-        [8422, 'Biden', 22291], #ME CD-1
-        [8422, 'Trump', 22290], #ME CD-1
-        [8423, 'Biden', 19490], #ME CD-2
-        [8423, 'Trump', 19489], #ME CD-2
-        [6231, 'Biden', 22098], #MD
-        [6231, 'Trump', 22097], #MD
-        [6230, 'Biden', 22110], #MA
-        [6230, 'Trump', 22109], #MA
-        [6233, 'Biden', 15709], #MI
-        [6233, 'Trump', 15708], #MI
-        [6234, 'Biden', 16002], #MN
-        [6234, 'Trump', 16001], #MN
-        [6236, 'Biden', 22229], #MS
-        [6236, 'Trump', 22228], #MS
-        [6235, 'Biden', 22063], #MO
-        [6235, 'Trump', 22062], #MO
-        [6237, 'Biden', 22127], #MT
-        [6237, 'Trump', 22126], #MT
-        [8718, 'Biden', 22138], #NE CD-2
-        [8718, 'Trump', 22137], #NE CD-2
-        [6244, 'Biden', 16010], #NV
-        [6244, 'Trump', 16009], #NV
-        [6241, 'Biden', 16004], #NH
-        [6241, 'Trump', 16003], #NH
-        [6242, 'Biden', 22061], #NJ
-        [6242, 'Trump', 22060], #NJ
-        [6243, 'Biden', 22036], #NM
-        [6243, 'Trump', 22035], #NM
-        [6245, 'Biden', 22162], #NY
-        [6245, 'Trump', 22161], #NY
-        [6238, 'Biden', 16006], #NC
-        [6238, 'Trump', 16005], #NC
-        [6239, 'Biden', 22266], #ND
-        [6239, 'Trump', 22265], #ND
-        [6246, 'Biden', 16008], #OH
-        [6246, 'Trump', 16007], #OH
-        [6247, 'Biden', 22177], #OK
-        [6247, 'Trump', 22176], #OK
-        [6249, 'Biden', 15705], #PA
-        [6249, 'Trump', 15704], #PA
-        [6251, 'Biden', 22140], #SC
-        [6251, 'Trump', 22139], #SC
-        [6253, 'Biden', 22079], #TN
-        [6253, 'Trump', 22078], #TN
-        [6254, 'Biden', 16949], #TX
-        [6254, 'Trump', 16948], #TX
-        [6255, 'Biden', 22077], #UT
-        [6255, 'Trump', 22076], #UT
-        [6256, 'Biden', 16012], #VA
-        [6256, 'Trump', 16011], #VA
-        [6258, 'Biden', 22114], #WA
-        [6258, 'Trump', 22113], #WA
-        [6260, 'Biden', 22175], #WV
-        [6260, 'Trump', 22174], #WV
-        [6259, 'Biden', 15703], #WI
-        [6259, 'Trump', 15702]]     #WI
- 
-# Create the pandas DataFrame key
-df = pd.DataFrame(states, columns = ['race_id', 'answer', 'Contract_ID'])
 
 # Pull in market data from PredictIt's API
 Predictit_URL = "https://www.predictit.org/api/marketdata/all/"
@@ -130,17 +44,46 @@ predictit_df = pd.DataFrame(data)
 # Update dataframe column names
 predictit_df.columns=['Market_ID','Market_Name','Contract_ID','Contract_Name','bestBuyYesCost','bestBuyNoCost','BestSellYesCost','BestSellNoCost']
 
+# Filter PredicitIt dataframe to presidential state markets/contracts
+predictit_df = predictit_df[predictit_df['Market_Name'].str.contains("Which party will win") & predictit_df['Market_Name'].str.contains("2020 presidential election?")]
+
+# Fix annoying typo (double space) in congressional district market names
+predictit_df['Market_Name'] = predictit_df['Market_Name'].str.replace('in the  2020','in the 2020')
+
+# Split Market_Name column into state name column
+start_string = "Which party will win"
+end_string = "in the 2020 presidential election?"
+predictit_df['a'], predictit_df['state'] = predictit_df['Market_Name'].str.split(start_string, 1).str
+predictit_df['state'], predictit_df['b'] = predictit_df['state'].str.split(end_string, 1).str
+del predictit_df['a']
+del predictit_df['b']
+
+# Create answer column from contract names
+predictit_df['answer'] = predictit_df['Contract_Name'].str.replace('Republican','Trump').str.replace('Democratic','Biden')
+
+# Strip trailing/leading whitespaces in answer and state columns
+predictit_df['state'] = predictit_df['state'].str.strip()
+predictit_df['answer'] = predictit_df['answer'].str.strip()
+
 # Pull in polling data from 538
 pres_polling = pd.read_csv('https://projects.fivethirtyeight.com/polls-page/president_polls.csv')
-#pres_polling = pres_polling.dropna(subset=['state'])
+pres_polling = pres_polling.dropna(subset=['state'])
 
 # Drop extraneous columns
 pres_polling = pres_polling.drop(['pollster_id', 'pollster','sponsor_ids','sponsors','display_name', 'pollster_rating_id', 'pollster_rating_name', 'fte_grade', 'sample_size', 'population', 'population_full', 'methodology', 'seat_number', 'seat_name', 'start_date', 'end_date', 'sponsor_candidate', 'internal', 'partisan', 'tracking', 'nationwide_batch', 'ranked_choice_reallocated', 'notes', 'url'], axis=1)
+
+# Standardize congressional district names in 538 with PredictIt
+pres_polling['state'] = pres_polling['state'].str.replace('Maine CD-1','ME-01')
+pres_polling['state'] = pres_polling['state'].str.replace('Maine CD-2','ME-02')
+pres_polling['state'] = pres_polling['state'].str.replace('Nebraska CD-2','NE-02')
 
 # Filter to most recent poll for Biden & Trump
 pres_polling['created_at'] = pd.to_datetime(pres_polling['created_at']) #convert 'created_at' to datetime
 recent_pres_polling = pres_polling.sort_values(by=['created_at']).drop_duplicates(['state', 'candidate_name'], keep='last')
 recent_pres_polling = recent_pres_polling[recent_pres_polling['answer'].isin(['Biden', 'Trump'])]
+
+# Rename 538 'pct' column to recent_poll
+recent_pres_polling = recent_pres_polling.rename({'pct': 'recent_poll'}, axis=1)
 
 # Pull in odds
 odds_df = pd.read_csv('https://raw.githubusercontent.com/mauricebransfield/predictit_538_odds/master/odds_state_presidential.csv', index_col=[0]) # error_bad_lines=False,
@@ -161,16 +104,16 @@ odds_df['answer'] = odds_df['answer'].str.replace('Democrat','Biden')
 # Drop columns with all nan values
 odds_df = odds_df.dropna(axis=1, how='all')
 
-# Merge key and 538 dataframes together
-df = pd.merge(df, recent_pres_polling, on=['race_id', 'answer'])
+# Merge PredictIt and odds dataframes together
+df = pd.merge(predictit_df, odds_df, on=['state', 'answer'], how='left')
 
-# Merge key and PredicitIt
-df = pd.merge(df, predictit_df, on=['Contract_ID'])
+# Merge 538 polls into new dataframe
+df = pd.merge(df, recent_pres_polling, on=['state', 'answer'], how='left')
 
-# Merge df and trump odds
-df = pd.merge(df, odds_df, on=['state', 'answer'], how='left')
+# Sort alphabetically by state
+df = df.reindex(df.state.sort_values(ascending=True).index)
 
-# Convert fractional odds to column of implied probability
+# Convert fractional odds to new column of implied probability
 # denominator / (denominator + numerator) = implied probability
 df['numerator'], df['denominator'] = df['betfair'].str.split('/', 1).str
 df['denominator'] = pd.to_numeric(df['denominator'], errors='coerce').fillna(0).astype(np.int64)
@@ -193,7 +136,7 @@ df['betfair-PredicitIt'] = df['betfair-PredicitIt'].round(2)
 #print out select columns
 print(df[['state', 
             'answer', 
-            'pct', 
+            'recent_poll', 
             'bestBuyYesCost', 
             'betfair',
             'betfair_imp_prob',
